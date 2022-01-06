5c2d3cbad89535b2df8c4d1e4e0753e86684b6cb
Michele Sciabarra <michele@sciabarra.com>
2022-01-06 12:10:17 +0000

    implemented wrappers for kube and kustomze

--- nuvolaris/kube.py
+++ nuvolaris/kube.py
@@ -1,16 +1,8 @@
-#import logging, importlib as imp
-#logging.basicConfig(level=logging.DEBUG)
-#import nuvolaris.kube as kube
-#imp.reload(kube)
-#imp.reload(tu)
-
+# this module wraps kubectl
 import subprocess
-import nuvolaris.testutil as testutil
-import os
 import json
 import logging
 
-mode = "apply"
 output = ""
 error = ""
 returncode = -1
@@ -21,9 +13,8 @@ returncode = -1
 # if you specify jsonpath it will filter and parse the json output
 # returns exceptions if errors
 def kubectl(*args, namespace="nuvolaris", jsonpath=None):
-    """
-    imp.reload(kube)
-    imp.reload(tu)
+    """Test kube
+    
     >>> import nuvolaris.kube as kube
     >>> import nuvolaris.testutil as tu
     >>> tu.grep(kube.kubectl("get", "ns"), "kube-system", field=0)
@@ -34,13 +25,13 @@ def kubectl(*args, namespace="nuvolaris", jsonpath=None):
     True
     >>> tu.catch(lambda: kube.kubectl("error"))
     <class 'Exception'> Error: flags cannot be placed before plugin name: -n
-    >>> kube.returncode, kube.error
-    (1, 'Error: flags cannot be placed before plugin name: -n\n')
+    >>> print(kube.returncode, kube.error.strip())
+    1 Error: flags cannot be placed before plugin name: -n
     """
-    cmd = ["kubectl", "-n", namespace] 
+    cmd = ["kubectl", "-n", namespace]
     cmd += list(args)
     if jsonpath:
-        cmd += ["-o", "jsonpath-as-json=%s" % jsonpath] 
+        cmd += ["-o", "jsonpath-as-json=%s" % jsonpath]
     logging.debug("command: %s", " ".join(cmd))
     # executing
     res = subprocess.run(cmd, capture_output=True)
@@ -56,50 +47,8 @@ def kubectl(*args, namespace="nuvolaris", jsonpath=None):
                 logging.debug("result: %s", json.dumps(parsed, indent=2))
                 return parsed
         else:
-            return output        
+            return output
     raise Exception(error)
-    
-
-# execute the kustomization of a folder under "deploy"
-# uses the mode variable to decide if kustomize, apply or delete
-# it generate a kustomization.yaml, adding the header 
-# then including all the files of a folder as resources
-# so you do not have to specify them
-# returns True or False, 
-# if True, check output for stdout and error for stderrs
-# if False, check error 
-def kustomize(where, what):
-    global mode, output, error
-    # prepare the kustomization
-    tgt = f"deploy/{where}/kustomization.yaml"
-    with open(tgt, "w") as f:
-        f.write("apiVersion: kustomize.config.k8s.io/v1beta1\nkind: Kustomization\n")
-        f.write(what)
-        f.write("resources:\n")
-        for file in os.listdir(f"deploy/{where}"):
-            if file != "kustomization.yaml":
-                f.write(f"- {file}\n")
-    
-    # generate command
-    cmd = ["kubectl"]
-    if mode == "kustomize":
-        cmd.append("kustomize")
-    elif mode == "delete":
-        cmd += ["delete", "-k"]
-    elif mode == "apply":
-        cmd += ["apply", "-k"]
-    else:
-        error = "kustomize.mode should be kustomize, delete or apply"
-        return False
-    cmd.append(f"deploy/{where}")
-    logging.debug(cmd)
-
-    # execute it
-    res = subprocess.run(cmd, capture_output=True)
-    logging.debug(res)
-    output = res.stdout.decode()
-    error = res.stderr.decode()
-    return res.returncode == 0
 
 
 
--- nuvolaris/testutil.py
+++ nuvolaris/testutil.py
@@ -1,28 +1,32 @@
 import re
 
+
 # takes a string, split in lines and search for the word (a re)
 # if field is a number, splits the line in fields separated by spaces and print the selected field
 # the output is always space trimmed for easier check
+
+
 def grep(input, word, field=None):
-    """
-    >>> import util
-    >>> util.grep("a\nb\nc\n", "b")
-    b
-    >>> util.grep(b"a\nb\n c\n", r"a|c")
-    a
-    c
-    """
-    try: input = input.decode()
-    except: pass
-    for line in input.split("\n"):
-        if re.search(word, line):
-            line = line.strip()
-            if not field is None:
-                try:
-                    line = line.split()[field]
-                except:
-                    line = "missing-field"
-            print(line)
+  r"""Test grep
+
+  >>> import nuvolaris.testutil as tu
+  >>> tu.grep("a\nb\nc\n", "b")
+  b
+  >>> tu.grep(b"a\nb\n c\n", r"a|c")
+  a
+  c
+  """
+  try: input = input.decode()
+  except: pass
+  for line in input.split("\n"):
+    if re.search(word, line):
+        line = line.strip()
+        if not field is None:
+            try:
+                line = line.split()[field]
+            except:
+                line = "missing-field"
+        print(line)
 
 # capture and print an excection
 def catch(f):
