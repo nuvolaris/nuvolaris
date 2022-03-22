20362f7fa571e4b2c6ca0edb646c0ea082e25231
Michele Sciabarra <michele@sciabarra.com>
2022-03-22 14:29:55 +0000

    init_database

--- nuvolaris/couchdb_init.py
+++ nuvolaris/couchdb_init.py
@@ -19,22 +19,84 @@ import os
 import requests as req
 from jinja2 import Environment, FileSystemLoader
 
+
 db_protocol   = os.environ.get("DB_PROTOCOL", "http")
-db_prefix     = os.environ.get("DB_PREFIX", "test_") 
+db_prefix     = os.environ.get("DB_PREFIX", "nuvolaris_") 
 db_host       = os.environ.get("DB_HOST", "localhost")
+
 db_username   = os.environ.get("COUCHDB_USER",  "whisk_admin")
 db_password   = os.environ.get("COUCHDB_PASSWORD", "some_passw0rd")
 db_port       = os.environ.get("DB_PORT", "5984")
-db_prefix     = "nuvolaris_"
 
-db_base = f"{db_protocol}://{db_username}:{db_password}@{db_host}:{db_port}/"
-db_auth = f"{db_base}{db_prefix}subjects"
+db_auth = req.auth.HTTPBasicAuth(db_username,db_password)
+db_base = f"{db_protocol}://{db_host}:{db_port}/{db_prefix}"
 
 loader = FileSystemLoader(["./nuvolaris/templates", "./nuvolaris/files"])
 env = Environment(loader=loader)
+ 
 
-def sample_request():
-  return req.get(db_base).text
+
+# check if database exists, return boolean
+def check_db(database):
+  global db_auth, db_base
+  url = f"{db_base}{database}"
+  r = req.head(url, auth=db_auth)
+  return r.status_code == 200
+ 
+# delete database, return true if ok
+def delete_db(database):
+  global db_auth, db_base
+  url = f"{db_base}{database}"
+  r = req.delete(url, auth=db_auth)
+  return r.status_code == 200
+
+# create db, return true if ok
+def create_db(database):
+  global db_auth, db_base
+  url = f"{db_base}{database}"
+  r = req.put(url, auth=db_auth) 
+  return r.status_code == 201
+
+
+# database="subjects"
+def recreate_db(database, recreate=False):
+  """
+  >>> from nuvolaris.couchdb_init import *
+  >>> check_db("subjects")
+  False
+  >>> create_db("subjects")
+  True
+  >>> check_db("subjects")
+  True
+  >>> create_db("subjects")
+  False
+  >>> delete_db("subjects")
+  True
+  >>> delete_db("subjects")
+  False
+  >>> check_db("subjects")
+  False
+  >>> recreate_db("subjects")
+  'recreate_db: created'
+  >>> check_db("subjects")
+  True
+  >>> recreate_db("subjects")
+  'recreate_db:'
+  >>> recreate_db("subjects", recreate=True)
+  'recreate_db: deleted created'
+  >>> delete_db("subjects")
+  True
+  """
+  msg = "recreate_db:"
+  exists = check_db(database)
+  if recreate and exists:
+    msg += " deleted"
+    delete_db(database)
+  if recreate or not exists:
+    msg += " created"
+    create_db(database)
+  return msg
+
 
 def sample_expand(username, password):
   return env.get_template("createUser.json").render(item={
