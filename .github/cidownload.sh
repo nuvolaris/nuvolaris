d10a52464e9cad3de1e1f671c9747b6033a94b5d
Michele Sciabarra <michele@sciabarra.com>
2022-06-12 18:18:16 +0100

    download release from nuvolaris-cli

---
+++ .github/cidownload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VER="${1:?version}"
+PRE="https://github.com/nuvolaris/nuvolaris-cli/releases/download"
+wget -nc $PRE/$VER/nuv-windows-amd64.zip
+wget -nc $PRE/$VER/nuv-windows-amd64.zip.md5
+for OS in linux darwin
+do for ARC in amd64 arm64
+   do wget -nc $PRE/$VER/nuv-$VER-$OS-$ARC.tar.gz
+      wget -nc $PRE/$VER/nuv-$VER-$OS-$ARC.tar.gz.md5
+   done
+done
+
+
