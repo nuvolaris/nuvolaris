c9541296cc5adf7ead6121f9062286fc18442dd4
Michele Sciabarra <michele@sciabarra.com>
2022-06-13 09:06:40 +0200

    fix windows download in release

--- .github/cidownload.sh
+++ .github/cidownload.sh
@@ -19,8 +19,8 @@
 TAG="${1:?version}"
 VER="${TAG#refs/*/}"
 PRE="https://github.com/nuvolaris/nuvolaris-cli/releases/download"
-wget -nc $PRE/$VER/nuv-windows-amd64.zip
-wget -nc $PRE/$VER/nuv-windows-amd64.zip.md5
+wget -nc $PRE/$VER/nuv-$VER-windows-amd64.zip
+wget -nc $PRE/$VER/nuv-$VER-windows-amd64.zip.md5
 for OS in linux darwin
 do for ARC in amd64 arm64
    do wget -nc $PRE/$VER/nuv-$VER-$OS-$ARC.tar.gz
