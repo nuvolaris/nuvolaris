363e597896b18142b250db3fcafb24993b92e788
Michele Sciabarra <michele@sciabarra.com>
2022-07-25 22:07:45 +0100

    preparing release v0.2.2

---
+++ test-cli.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+VERSION="${1:?version}"
+EXT="tar.gz"
+EXTRACT="tar xzvf"
+
+case "$(arch)" in
+  x86_64) ARCH=amd64 ;;
+  arm64) ARCH=arm64 ;;
+  *) echo "unknown architecture"; exit 1 ;;
+esac
+
+case "$(uname)" in
+    Darwin) OS="darwin" ;;
+    Linux) OS="linux" ;;
+    MINGW*)
+        OS="windows"
+        EXT="zip"
+        ARCH="amd64"
+        EXTRACT="unzip"
+    ;;
+    *) echo "unknown platform"; exit 1 ;;
+esac
+
+URL="https://github.com/nuvolaris/nuvolaris-cli/releases/download/$VERSION/nuv-$VERSION-$OS-$ARCH.$EXT"
+
+curl -sL $URL >dld.$EXT
+$EXTRACT "dld.$EXT"
+
+./nuv devcluster destroy
+./nuv setup --devcluster
+
+echo 'function main(args) { return { "body": "OK" } }' >ok.js
+./nuv action create ok ok.js --web true
+URL="$(./nuv url ok)"
+
+if curl -sL $URL | grep OK
+then echo "SUCCESS"; exit 0
+else echo "FAILED" ; exit 1
+fi
