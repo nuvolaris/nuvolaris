5e4afcb10951b58e6fb7d091c0106fa4aee13329
Michele Sciabarra <michele@nuvolaris.io>
2023-08-12 09:31:58 +0100

    license

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,4 +1,21 @@
-    apt-get update && apt-get -y upgrade &&\
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+apt-get update && apt-get -y upgrade &&\
     apt-get -y install curl wget gpg zip unzip software-properties-common apt-utils unzip vim silversearcher-ag
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor > /usr/share/keyrings/docker-archive-keyring.gpg &&\
     wget -O- https://apt.corretto.aws/corretto.key | apt-key add -
